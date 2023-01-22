f6f87913a129ef47d861f320b33675f2ddd819ee
Anna Zhilina <azhilina@tradingview.com>
2023-01-22 20:00:35 +0300

    stage-5

--- hangman.py
+++ hangman.py
@@ -1,16 +1,33 @@
 import random
 correct_word = random.choice(['python', 'java', 'swift', 'javascript'])
 hint_word = ''
+new_hint = ''
 for i in range(len(correct_word)):
-    if i < 3:
-        hint_word = hint_word + correct_word[i]
-    else:
         hint_word = hint_word + '-'
+attempts = 8
 
-print("""H A N G M A N
-Guess the word: """, hint_word)
-word = input()
-if (word == correct_word):
-    print('You survived!')
-else:
-    print('You lost!')
+print("H A N G M A N")
+print('')
+while(attempts > 0):
+    print(hint_word)
+    letter = input('Input a letter: ')
+    if(letter in correct_word):
+        new_hint = ''
+        i = 0
+        while i < len(correct_word):
+            if letter == correct_word[i]:
+                new_hint = new_hint + correct_word[i]
+                i = i + 1
+            else:
+                new_hint = new_hint + hint_word[i]
+                i = i + 1
+        hint_word = new_hint
+        print('')
+        attempts = attempts - 1
+    else:
+        print("That letter doesn't appear in the word.")
+        print('')
+        attempts = attempts - 1
+
+
+print('Thanks for playing!')
