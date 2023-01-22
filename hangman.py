320afdf261011bd51ee4555f5cb6f114a8bb8b57
Anna Zhilina <azhilina@tradingview.com>
2023-01-22 17:58:03 +0300

    update to stage-4

--- hangman.py
+++ hangman.py
@@ -1,8 +1,14 @@
 import random
 correct_word = random.choice(['python', 'java', 'swift', 'javascript'])
+hint_word = ''
+for i in range(len(correct_word)):
+    if i < 3:
+        hint_word = hint_word + correct_word[i]
+    else:
+        hint_word = hint_word + '-'
 
 print("""H A N G M A N
-Guess the word: """)
+Guess the word: """, hint_word)
 word = input()
 if (word == correct_word):
     print('You survived!')
